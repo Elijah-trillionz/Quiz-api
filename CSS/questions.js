67f978c574f448b10c9b02b641761b8d76a4e4d6
Elijah Trillionz <60356516+Elijah-trillionz@users.noreply.github.com>
2020-11-09 22:37:48 +0100

    CSS questions
    CSS questions

---
+++ CSS/questions.js
@@ -0,0 +1,14 @@
+// leave first question as a sample
+// CSS questions only
+// strictly CSS
+
+questions: [
+{
+  id: 1, // numbers only
+  quest: 'Question', // string only
+  ans: 'option 3', // string only
+  options: ['option 1', 'option 2', 'option 3', 'option4'], // array of strings or numbers only
+  src: 'John Doe', // string
+  srcURL: 'https://johndoe.com', // string only (https:// or http:// compulsory & domain name (.org, .com etc)).
+},
+]
