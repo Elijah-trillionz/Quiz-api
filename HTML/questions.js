be8c0bedb589b47dfe509c063d4e096492a4cbc8
Elijah Trillionz <60356516+Elijah-trillionz@users.noreply.github.com>
2020-11-09 22:35:02 +0100

    HTML questions
    HTML questions

---
+++ HTML/questions.js
@@ -0,0 +1,13 @@
+// leave first question as a sample
+// HTML questions only
+// strictly HTML
+questions: [
+{
+  id: 1, // numbers only
+  quest: 'Question', // string only
+  ans: 'option 3', // string only
+  options: ['option 1', 'option 2', 'option 3', 'option4'], // array of strings or numbers only
+  src: 'John Doe', // string
+  srcURL: 'https://johndoe.com', // string only (https:// or http:// compulsory & domain name (.org, .com etc)).
+},
+]
