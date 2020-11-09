d20530ba082789c74bc48083f65b5ff0b23b901d
Elijah Trillionz <60356516+Elijah-trillionz@users.noreply.github.com>
2020-11-09 09:21:18 -1200

    Update questions.js

--- questions.js
+++ questions.js
@@ -6,6 +6,6 @@ questions: [
   ans: 'option 3', // string only
   options: ['option 1', 'option 2', 'option 3', 'option4'], // array of strings or numbers only
   src: 'John Doe', // string
-  srcURL: 'https://johndoe.com', // string only (https:// or http:// compulsory).
+  srcURL: 'https://johndoe.com', // string only (https:// or http:// compulsory & domain name (.org, .com etc)).
 },
 ]
